cfb63c013455860a86fce36a18a0bf982f5fc3a7
Ion Bengoechea González <ion.bengoechea@3aside.local>
2018-08-17 13:13:11 +0200

    player background image fix

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -6,17 +6,18 @@ import walkSprite from './player.png'
 function Player(props) {
     return (
         <div
+        
         style={{
             position: 'relative',
             top: props.position[1],
             left: props.position[0],
-            backgroundImage: `url('${walkSprite})`,
+            backgroundImage: `url('${walkSprite}`,
             backgroundPosition: '0 0',
             width: `40px`,
             height: `40px`
         }}
         
-        /> 
+        ></div> 
         
     )
 }
