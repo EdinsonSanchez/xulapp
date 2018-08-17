87fc116a00d99f022537c3f5ff59212bde0026e9
ilaydagurbak <ilaydagurbak@gmail.com>
2018-08-17 12:44:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Spritesheet from 'react-responsive-spritesheet';
+import React, { Component } from 'react'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Spritesheet
-          className={`my-element__class--style`}
-          image={`https://raw.githubusercontent.com/danilosetra/react-responsive-spritesheet/master/assets/images/examples/sprite-image-horizontal.png`}
-          widthFrame={420}
-          heightFrame={500}
-          steps={14}
-          fps={10}
-          autoplay={true}
-          loop={true}
-        />
-      </div>
-    );
+     <div></div>
+    )
   }
 }
 
-export default App;
+export default App
 
 
 
